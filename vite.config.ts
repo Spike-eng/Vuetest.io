628e96938247325291200b1f4dedfb5b066e365a
Spike <spike@example.com>
2026-04-03 23:01:42 +0300

    modified:   .gitignore modified:   vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,5 @@ import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
   plugins: [vue()],
-  base: 'https://spike-eng.github.io/Vuetest.io/', // 👈 ВАЖНО
+  base: '/spike-eng.github.io', // 👈 ВАЖНО
 })
